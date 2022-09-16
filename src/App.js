a438844ecc78e26ece14d39a98d018403610a08a
Gustavo-Targino <gustavo.targinocc@hotmail.com>
2022-09-16 14:54:22 -0300

    pasta routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <ChuckNorris />
+      <DadJoke/>
     </>
   );
 }
---
+++ src/routes/dadJoke.page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DadJokePage = () => {
+  return <div>dad joke here</div>;
+};
+
+export default DadJokePage;
